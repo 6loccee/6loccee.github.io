33ce2f6aa1e6e26def930e25c03dd605c4ec6eaa
6loccee <martiin17jr@gmail.com>
2024-06-21 07:35:35 +0200

    feat: add the colors to the basic tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#121212',
+        blue: '#10a2f5',
+        green: '#24d05a',
+        red: '#e4002b',
+      },
+    },
   },
   plugins: [],
 }
