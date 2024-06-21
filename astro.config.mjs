6fc1fbddb72f154794915cfe272348f0dd5b836f
6loccee <martiin17jr@gmail.com>
2024-06-21 21:47:48 +0200

    feat: add integrations for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+
+import webmanifest from 'astro-webmanifest'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://6loccee.github.io',
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt(),
+    webmanifest({
+      name: 'Martin Aranda ─ Dev',
+      short_name: 'Portfolio',
+      start_url: '/',
+      background_color: '#121212',
+    }),
+  ],
 })
