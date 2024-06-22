7fd4169af7aa25431263ed3949f5cbf2d7c3bf34
6loccee <martiin17jr@gmail.com>
2024-06-23 00:28:26 +0200

    feat: reorganize project and add minimal details

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,8 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        black: '#121212',
-        blue: '#10a2f5',
-        green: '#24d05a',
-        red: '#e4002b',
-        gray: '#1d1d1d',
-        darkGray: '#171717',
-      },
       fontFamily: {
-        inter: ['Inter'],
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
