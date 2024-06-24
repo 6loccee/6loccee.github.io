7145a9296be501aa14a1f2cd839223f1dcaf93fc
6loccee <martiin17jr@gmail.com>
2024-06-24 04:27:10 +0200

    feat: improving the toggle theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,13 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'background-color': 'hsl(var(--background-color))',
+        'primary-color': 'hsl(var(--text-color))',
+        'secondary-color': 'hsl(var(--footer-color))',
+        'underline-color': 'hsl(var(--underline-color))',
+        'underline-hover-color': 'hsl(var(--footer-hover-color))',
+      },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
