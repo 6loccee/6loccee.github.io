719a882e6160acd058f1a876420597d7829cfbbd
6loccee <martiin17jr@gmail.com>
2024-06-25 22:18:21 +0200

    feat: new feature to get what Im listening to on spotify

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { SpotifyApi } from '@spotify/web-api-ts-sdk'
+
 export function calculateAge(dateOfBirthStr: string): number {
   const dateParts = dateOfBirthStr.split('/')
   const birthDay = Number.parseInt(dateParts[0], 10)
@@ -17,4 +19,45 @@ export function calculateAge(dateOfBirthStr: string): number {
   age = hasBirthdayPassed ? age : age - 1
 
   return age
+}
+
+async function getAccessToken(
+  clientID: string,
+  clientSecret: string,
+  refreshToken: string,
+) {
+  const basic = Buffer.from(`${clientID}:${clientSecret}`).toString('base64')
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+    }),
+  })
+
+  return await response.json()
+}
+
+export async function getCurrentlyPlayed(
+  clientID: string,
+  clientSecret: string,
+  refreshToken: string,
+): Promise<string | null> {
+  const accessToken = await getAccessToken(clientID, clientSecret, refreshToken)
+  const sdk = SpotifyApi.withAccessToken(clientID, {
+    ...accessToken,
+    refresh_token: refreshToken,
+  })
+  const response = await sdk.player.getCurrentlyPlayingTrack()
+
+  if (!response || !response.item) return null
+
+  const { name, artists } = response.item
+  const artistNames = artists.map(artist => artist.name).join(', ')
+
+  return `${name} - ${artistNames}`
 }
