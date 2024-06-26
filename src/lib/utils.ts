f0982c3c8a8cc75cb6329898dee66b2367acbbee
6loccee <martiin17jr@gmail.com>
2024-06-26 09:52:36 +0200

    feat: add utilities to change the language

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,8 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
 export function calculateAge(dateOfBirthStr: string): number {
   const dateParts = dateOfBirthStr.split('/')
   const birthDay = Number.parseInt(dateParts[0], 10)
@@ -17,4 +22,21 @@ export function calculateAge(dateOfBirthStr: string): number {
   age = hasBirthdayPassed ? age : age - 1
 
   return age
+}
+
+export function getTranslationFile(locale = 'es') {
+  const data = fs.readFileSync(
+    path.join(__dirname, '..', `langs/${locale.slice(0, 2)}.json`),
+    'utf-8',
+  )
+  return data
+}
+
+export function t(key: string): string | undefined {
+  const keys = key.split('.')
+  const object = JSON.parse(getTranslationFile())
+
+  return keys.reduce((result, key) => {
+    return result?.[key]
+  }, object)
 }
