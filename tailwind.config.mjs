4df23daf1b621607f33534d4e1a2577c5feeb69c
6loccee <martiin17jr@gmail.com>
2024-06-27 08:17:53 +0200

    feat: add new colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,13 @@ export default {
         'underline-hover-color': 'hsl(var(--footer-hover-color))',
         'card-color': 'hsl(var(--card-color))',
         'card-hover-color': 'hsl(var(--card-hover-color))',
+        'underline-color1': 'hsl(var(--underline-color1) / 0.2)',
+        'underline-color2': 'hsl(var(--underline-color2) / 0.2)',
+        'underline-color3': 'hsl(var(--underline-color3) / 0.2)',
+        'underline-color4': 'hsl(var(--underline-color4) / 0.2)',
+        'underline-color5': 'hsl(var(--underline-color5) / 0.2)',
+        'underline-color6': 'hsl(var(--underline-color6) / 0.2)',
+        'underline-color7': 'hsl(var(--underline-color7) / 0.2)',
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
